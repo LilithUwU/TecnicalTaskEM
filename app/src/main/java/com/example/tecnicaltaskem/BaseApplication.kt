47e47296c77c3095c0626dfe691c6987900e5a76
Lilith <lilith.takiryan@gmail.com>
2026-03-17 22:34:00 +0400

    refine UI styling and layout configurations

--- app/src/main/java/com/example/tecnicaltaskem/BaseApplication.kt
+++ app/src/main/java/com/example/tecnicaltaskem/BaseApplication.kt
@@ -1,6 +1,7 @@
 package com.example.tecnicaltaskem
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.data.di.dataModule
 import com.example.domain.di.domainModule
 import com.example.tecnicaltaskem.di.appModule
@@ -10,6 +11,9 @@ import org.koin.core.context.GlobalContext.startKoin
 class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
         startKoin {
             androidContext(this@BaseApplication)
             modules(listOf(dataModule, domainModule, appModule))
